f9da33a53f440413250141d56b8228e1fa644e9c
Muhammad Faiz Irfan Bin Ariffin <85354814+Faiz-Irfan@users.noreply.github.com>
2025-06-07 10:07:31 +0800

    log detail updated

--- resources/views/user/log-detail.blade.php
+++ resources/views/user/log-detail.blade.php
@@ -31,12 +31,38 @@
                         <th>Subject ID</th>
                         <td>{{ $log->subject_id ?? '-' }}</td>
                     </tr>
-                    <tr>
-                        <th>Properties (Changes)</th>
-                        <td>
-                            <pre class="mb-0">{{ json_encode($log->properties, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
-                        </td>
-                    </tr>
+                    @if(isset($log->properties['attributes']) && isset($log->properties['old']))
+                        <tr>
+                            <th>Changed Attributes</th>
+                            <td>
+                                <table class="table table-sm table-bordered mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th>Field</th>
+                                            <th>Old Value</th>
+                                            <th>New Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($log->properties['attributes'] as $key => $newValue)
+                                            <tr>
+                                                <td>{{ $key }}</td>
+                                                <td>{{ $log->properties['old'][$key] ?? '-' }}</td>
+                                                <td>{{ $newValue }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    @else
+                        <tr>
+                            <th>Properties (Changes)</th>
+                            <td>
+                                <pre class="mb-0">{{ json_encode($log->properties, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
+                            </td>
+                        </tr>
+                    @endif
                 </table>
                 <a href="{{ url()->previous() }}" class="btn btn-secondary">Back</a>
             </div>
